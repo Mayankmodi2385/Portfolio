b74f591a47e9a8f96ade712815e1a49a429296af
Mayankmodi2385 <mayankmodi7126@gmail.com>
2026-05-01 17:54:19 +0530

    add the image

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,7 +1,23 @@
-import Scene from "./Scene";
-
-const CharacterModel = () => {
-  return <Scene />;
+const Character = () => {
+  return (
+    <div className="character-model">
+      <div className="character-rim" />
+      <img
+        src="/mayank.png"
+        alt="Mayank Modi"
+        style={{
+          height: "85%",
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: 2,
+          filter:
+            "drop-shadow(0 0 30px rgba(0,255,247,0.6)) drop-shadow(0 0 80px rgba(189,0,255,0.3))",
+        }}
+      />
+    </div>
+  );
 };
 
-export default CharacterModel;
+export default Character;
